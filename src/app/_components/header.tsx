f462eff8a43a927fd31d02192cec16788ac41fe1
Gui <slgprogrammingguy@protonmail.com>
2025-10-26 15:15:13 -0300

    feat/Underline aparece quando mouse passa por cima das opções do menu

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,10 +1,10 @@
-"use Client";
+"use client"
 
 export function Header() {
   return (
     <header className="bg-black">
       <div className="mx-8 flex justify-between py-2">
-        <img src={"public/icon.png"} alt="Logo" />
+      <img src={} alt="Logo"/>
         <div className="flex space-x-2">
           <button className="border-orange text-orange rounded-lg border-2 bg-black p-1 px-5 font-bold hover:bg-gray-800">
             Log In
@@ -16,11 +16,11 @@ export function Header() {
       </div>
       <div className="bg-orange flex justify-between px-4 py-1">
         <div className="flex space-x-5 font-bold text-white">
-          <a>Games</a>
-          <a>News</a>
-          <a>Forum</a>
-          <a>Lists</a>
-          <a>Help</a>
+          <a className="hover:underline">Games</a>
+          <a className="hover:underline">News</a>
+          <a className="hover:underline">Forum</a>
+          <a className="hover:underline">Lists</a>
+          <a className="hover:underline">Help</a>
         </div>
         <input
           type="text"
